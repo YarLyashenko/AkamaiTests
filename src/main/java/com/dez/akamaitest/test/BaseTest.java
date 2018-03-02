1aa5969ca0b427324da6c33d4e95c48501bdfc77
Dmytro Shablia <12109878+dezik@users.noreply.github.com>
2018-03-02 13:39:37 +0100

    BaseTest fix

--- src/main/java/com/dez/akamaitest/test/BaseTest.java
+++ src/main/java/com/dez/akamaitest/test/BaseTest.java
@@ -1,40 +1,19 @@
 package com.dez.akamaitest.test;
 
 import com.dez.akamaitest.listeners.ConsoleLoggerListener;
-import com.dez.akamaitest.listeners.ScreenShotOnFailListener;
+import com.dez.akamaitest.listeners.CustomListener;
 import com.dez.akamaitest.pages.SearchJobPage;
 import io.qameta.allure.Step;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.*;
 
-import java.util.concurrent.TimeUnit;
-
+import static com.dez.akamaitest.utils.DriverManager.getDriver;
 import static org.assertj.core.api.Java6Assertions.assertThat;
 
-@Listeners({ScreenShotOnFailListener.class, ConsoleLoggerListener.class})
-public abstract class BaseTest {
+@Listeners({CustomListener.class, ConsoleLoggerListener.class})
+abstract class BaseTest {
 
     private static final String SEARCH_JOB_URL = "http://akamaijobs.referrals.selectminds.com/";
-    private final ThreadLocal<WebDriver> driver = ThreadLocal.withInitial(ChromeDriver::new);
-
-    @BeforeClass
-    @Step("Configure WebDriver before class")
-    public void classSetUp() {
-        getDriver().manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
-        getDriver().manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-    }
-
-    @AfterClass
-    @Step("Close driver.")
-    public void classTearDown() {
-        getDriver().quit();
-    }
-
-    public WebDriver getDriver() {
-        return driver.get();
-    }
 
     @Step("Open 'Search Job' page")
     SearchJobPage openSearchJobPage() {
