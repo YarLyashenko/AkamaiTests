a15d96e6c822ac0d732584498e33148f5030b6b7
Dmytro Shablia <12109878+dezik@users.noreply.github.com>
2018-03-02 13:41:33 +0100

    DriverManager impl

---
+++ src/main/java/com/dez/akamaitest/utils/DriverManager.java
@@ -0,0 +1,15 @@
+package com.dez.akamaitest.utils;
+
+import org.openqa.selenium.WebDriver;
+
+public class DriverManager {
+    private static final ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+
+    public static WebDriver getDriver() {
+        return driver.get();
+    }
+
+    public static void setDriver(WebDriver driver) {
+        DriverManager.driver.set(driver);
+    }
+}
