14bd02f663e0c7dbd1d0d908bc5063cc2ec1e0fc
Dmytro Shablia <12109878+dezik@users.noreply.github.com>
2018-03-02 09:41:01 +0100

    Added getPageSource() method

--- src/main/java/com/dez/akamaitest/pages/BasePage.java
+++ src/main/java/com/dez/akamaitest/pages/BasePage.java
@@ -1,9 +1,15 @@
 package com.dez.akamaitest.pages;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 abstract class BasePage {
     WebDriver driver;
     WebDriverWait wait;
+    
+    @Step("Get page source")
+    public String getPageSource() {
+        return driver.getPageSource();
+    }
 }
