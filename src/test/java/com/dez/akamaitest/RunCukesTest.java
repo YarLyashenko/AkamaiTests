bd21e8f6ab39a5ca52b6bdd483500ecb43d5f8b1
dezik <dezikk@gmail.com>
2018-03-03 20:31:11 +0100

    small fixes

--- src/test/java/com/dez/akamaitest/RunCukesTest.java
+++ src/test/java/com/dez/akamaitest/RunCukesTest.java
@@ -5,7 +5,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 import org.testng.annotations.Listeners;
 
-@CucumberOptions(features = "src/main/resources/features/")
+@CucumberOptions(features = "src/test/resources/features/")
 @Listeners({CustomListener.class})
 public class RunCukesTest extends AbstractTestNGCucumberTests {
 }
