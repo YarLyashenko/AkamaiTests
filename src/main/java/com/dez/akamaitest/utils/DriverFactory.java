df26249dacd138f91db4d8077f734055da86280f
dezik <dezikk@gmail.com>
2018-03-05 21:10:04 +0100

    Added possibility to run in grid

--- src/main/java/com/dez/akamaitest/utils/DriverFactory.java
+++ src/main/java/com/dez/akamaitest/utils/DriverFactory.java
@@ -4,22 +4,29 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class DriverFactory {
     public static WebDriver createInstance(String browserName) {
-        WebDriver driver = null;
-        if (browserName.toLowerCase().contains("firefox")) {
-            driver = new FirefoxDriver();
-            return driver;
+        switch (browserName) {
+            case "firefox":
+                return new FirefoxDriver();
+            case "ie":
+                return new InternetExplorerDriver();
+            case "chrome":
+                return new ChromeDriver();
+            case "grid":
+                try {
+                    return new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), DesiredCapabilities.chrome());
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+            default:
+                return new ChromeDriver();
         }
-        if (browserName.toLowerCase().contains("ie")) {
-            driver = new InternetExplorerDriver();
-            return driver;
-        }
-        if (browserName.toLowerCase().contains("chrome")) {
-            driver = new ChromeDriver();
-            return driver;
-        }
-        return driver;
     }
 }
